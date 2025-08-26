82791e2af657317073574d534aad9a968da9c691
Parham Behzad <parham.behzad@gmail.com>
2025-08-26 12:40:12 +0200

    00000000

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // Add this line
+  base: '/zscorenotes.github.ioe/', 
   plugins: [react()],
   server: {
     allowedHosts: true
