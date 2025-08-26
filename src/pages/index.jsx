d0210bc735046a06f3b7b5b922af02fa3a97617c
Parham Behzad <parham.behzad@gmail.com>
2025-08-26 12:59:46 +0200

    Configure for GitHub Pages deployment

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,11 @@
+// src/pages/index.jsx
+
 import Layout from "./Layout.jsx";
-
 import Home from "./Home";
-
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
-const PAGES = {
-    
-    Home: Home,
-    
-}
+// ... (keep the PAGES and _getCurrentPage function as is)
 
-function _getCurrentPage(url) {
-    if (url.endsWith('/')) {
-        url = url.slice(0, -1);
-    }
-    let urlLastPart = url.split('/').pop();
-    if (urlLastPart.includes('?')) {
-        urlLastPart = urlLastPart.split('?')[0];
-    }
-
-    const pageName = Object.keys(PAGES).find(page => page.toLowerCase() === urlLastPart.toLowerCase());
-    return pageName || Object.keys(PAGES)[0];
-}
-
-// Create a wrapper component that uses useLocation inside the Router context
 function PagesContent() {
     const location = useLocation();
     const currentPage = _getCurrentPage(location.pathname);
@@ -31,20 +13,19 @@ function PagesContent() {
     return (
         <Layout currentPageName={currentPage}>
             <Routes>            
-                
-                    <Route path="/" element={<Home />} />
-                
-                
+                <Route path="/" element={<Home />} />
                 <Route path="/Home" element={<Home />} />
-                
             </Routes>
         </Layout>
     );
 }
 
 export default function Pages() {
+    // Replace 'your-repo-name' with your actual repository name
+    const repoName = 'zscorenotes.github.io';
+
     return (
-        <Router>
+        <Router basename={`/${repoName}/`}>
             <PagesContent />
         </Router>
     );
