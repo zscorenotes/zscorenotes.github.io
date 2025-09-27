2f34765b6379d4a1f4fec013cc9b09c49c04386b
Parham Behzad <parham.behzad@gmail.com>
2025-09-27 18:40:21 +0200

    githero36666

--- scripts/ignore-build-step.sh
+++ scripts/ignore-build-step.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-if git diff --name-only HEAD~1 HEAD | grep -v '^content-data/\|^public/images/' > /dev/null; then
-echo "Code changed. Building."
-exit 0
-else
-echo "Only content changed. Skipping."
-exit 1
-fi
+if git diff --name-only HEAD~1 HEAD | grep -v '^content-data/\|^public/images/' > /dev/null; then echo "Code changed. Building."; exit 0; else echo "Only content changed. Skipping."; exit 1; fi
 
