8f595e21de319da3acf83c6654c2d7c8290cd6c0
Parham Behzad <parham.behzad@gmail.com>
2026-02-03 12:33:13 +0100

    rebuild

--- src/components/ModernContact.jsx
+++ src/components/ModernContact.jsx
@@ -98,7 +98,7 @@ export default function ModernContact() {
             >
               more →
             </button> 
-          )} */}
+          )} */} 
         </div>
       </section>
     </>
